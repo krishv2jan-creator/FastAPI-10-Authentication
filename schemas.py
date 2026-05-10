f47b282cc66411d110bcabef13f119b4b5b09f53
Krish Jan <krishv2jan@gmail.com>
2026-05-10 10:05:27 +0000

    Video 10 completed

--- schemas.py
+++ schemas.py
@@ -8,7 +8,20 @@ class UserBase(BaseModel):
 
 
 class UserCreate(UserBase):
-    pass
+    password: str = Field(min_length=8)
+
+
+class UserPublic(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    username: str
+    image_file: str | None
+    image_path: str
+
+
+class UserPrivate(UserPublic):
+    email: EmailStr
 
 
 class UserUpdate(BaseModel):
@@ -17,12 +30,9 @@ class UserUpdate(BaseModel):
     image_file: str | None = Field(default=None, min_length=1, max_length=200)
 
 
-class UserResponse(UserBase):
-    model_config = ConfigDict(from_attributes=True)
-
-    id: int
-    image_file: str | None
-    image_path: str
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
 class PostBase(BaseModel):
@@ -45,4 +55,4 @@ class PostResponse(PostBase):
     id: int
     user_id: int
     date_posted: datetime
-    author: UserResponse
+    author: UserPublic
